d0c621141dbba717a96495238d8b34002f982668
Lubomir Cuhel <palmyman@z68.(none)>
2013-06-24 23:45:47 +0200

    dual colored social incons in footer

---
+++ assets/themes/twitter/js/social-icons.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+  $("footer img").mouseover(function(){
+  	var oldsrc = $(this).attr('src');
+  	var newsrc = oldsrc;
+  	newsrc = newsrc.replace('black', 'color');
+	$(this).attr('src', newsrc);
+  });
+  $("footer img").mouseout(function(){
+  	var oldsrc = $(this).attr('src');
+  	var newsrc = oldsrc;
+  	newsrc = newsrc.replace('color', 'black');
+	$(this).attr('src', newsrc);
+  });
+});
