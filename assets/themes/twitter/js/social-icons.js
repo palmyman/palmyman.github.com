d8c075d914b87d901d830efcd6917a69706bcaeb
Lubomir Cuhel <palmyman@z68.(none)>
2013-06-26 23:58:54 +0200

    jquery update, new rolldown div

--- assets/themes/twitter/js/social-icons.js
+++ assets/themes/twitter/js/social-icons.js
@@ -3,15 +3,21 @@ $(document).ready(function(){
   	var oldsrc = $(this).attr('src');
   	var newsrc = oldsrc;
   	newsrc = newsrc.replace('black', 'color');
-	$(this).attr('src', newsrc);
+	  $(this).attr('src', newsrc);
   });
   $("footer img").mouseout(function(){
   	var oldsrc = $(this).attr('src');
   	var newsrc = oldsrc;
   	newsrc = newsrc.replace('color', 'black');
-	$(this).attr('src', newsrc);
+	  $(this).attr('src', newsrc);
   });
   
-    $("p#hiding").delay(1500).slideUp(800);
-  
+  $("p#hiding").after('<div id="open" style="display: none;">roll down</div>');
+  $("p#hiding").delay(2500).slideUp('slow', function(){    
+    $("div#open").slideDown('slow');
+  });
+  $("div#open").mouseover(function(){    
+    $("p#hiding").slideDown('slow');
+    $("div#open").slideUp('slow');
+  });
 });
