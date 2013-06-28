9a059170dcbd7cd430274eece01f33ff9b8d70af
Lubomir Cuhel <palmyman@vaio-palmyman.(none)>
2013-06-28 15:01:23 +0200

    hiding pragraph onMouseOver insted of click

--- assets/themes/twitter/js/social-icons.js
+++ assets/themes/twitter/js/social-icons.js
@@ -20,7 +20,7 @@ $(document).ready(function(){
   $("p#hiding").delay(2500).slideUp('slow', function(){    
     $("p#open").slideDown('slow');
   });
-  $("p#open").click(function(){
+  $("p#open").onMouseOver(function(){
     $("p#open").slideUp('slow', function(){
       $("p#hiding").slideDown('slow');  
     });
