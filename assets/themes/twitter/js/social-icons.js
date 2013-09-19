66957c47b47d0268384a6eeb031bb829a8b91d6e
Lubomir Cuhel <palmyman@z68.(none)>
2013-09-19 15:38:15 +0200

    zmena metody meneni social-icons na ciste css

--- assets/themes/twitter/js/social-icons.js
+++ assets/themes/twitter/js/social-icons.js
@@ -1,17 +1,4 @@
 $(document).ready(function(){
-  $("footer img").mouseover(function(){
-  	var oldsrc = $(this).attr('src');
-  	var newsrc = oldsrc;
-  	newsrc = newsrc.replace('black', 'color');
-	  $(this).attr('src', newsrc);
-  });
-  $("footer img").mouseout(function(){
-  	var oldsrc = $(this).attr('src');
-  	var newsrc = oldsrc;
-  	newsrc = newsrc.replace('color', 'black');
-	  $(this).attr('src', newsrc);
-  });
-  
   var hiding = $("p#hiding").text();
   var lenght = hiding.indexOf(' ', 85);
   hiding = hiding.substr(0, lenght) + '<a> ..read more.</a>';
@@ -20,7 +7,7 @@ $(document).ready(function(){
   $("p#hiding").delay(2500).slideUp('slow', function(){    
     $("p#open").slideDown('slow');
   });
-  $("p#open").mouseover(function(){
+  $("p#open").on("mouseover", function(){
     $("p#open").slideUp('slow', function(){
       $("p#hiding").slideDown('slow');  
     });
